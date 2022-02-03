a89c6edf889bdd54ed84f05d283bc309253881e1
Israel P. da Silva <israel4p@gmail.com>
2022-02-03 09:33:31 -0300

    Configurações comentadas

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,14 +1,14 @@
 local cmd = vim.cmd
 
 require('nvim-tree').setup {
-  open_on_setup = false,
-  view = {
-      width = 32,
-      auto_resize = true
-  },
-  filters = {
-      dotfiles = true,
-      custom = { '.git', 'node_modules', '.cache', '.bin' },
-  },
+--  open_on_setup = false,
+--  view = {
+--      width = 32,
+--      auto_resize = true
+--  },
+--  filters = {
+--      dotfiles = true,
+--      custom = { '.git', 'node_modules', '.cache', '.bin' },
+--  },
 }
 
