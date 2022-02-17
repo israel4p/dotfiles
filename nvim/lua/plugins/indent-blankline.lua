ba331e6b553997c6e7adc89373a283a3ddcf7306
Israel P. da Silva <israel4p@gmail.com>
2022-02-17 10:22:39 -0300

    Guia de indentação somente para arquivos python, html e css

--- nvim/lua/plugins/indent-blankline.lua
+++ nvim/lua/plugins/indent-blankline.lua
@@ -1,5 +1,6 @@
 vim.opt.list = true
 vim.opt.listchars:append("eol:↴")
+vim.cmd [[ let g:indent_blankline_filetype = [ 'python', 'html', 'css' ] ]]
 
 require("indent_blankline").setup {
     show_end_of_line = true,
