42551e2857063e7621b23392694e6802c5aa1278
Israel Pereira <israel4p@gmail.com>
2022-04-13 20:27:29 -0300

    Não lista aquivos e diretórios ocultos por padrão

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,14 +1,9 @@
 local cmd = vim.cmd
 
 require('nvim-tree').setup {
---  open_on_setup = false,
---  view = {
---      width = 32,
---      auto_resize = true
---  },
---  filters = {
---      dotfiles = true,
---      custom = { '.git', 'node_modules', '.cache', '.bin' },
---  },
+  filters = {
+    dotfiles = true,
+    custom = {},
+    exclude = {}
+  },
 }
-
