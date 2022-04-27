2f81f93f0f75382f7626109dbffbb39da5993da5
Israel P. da Silva <israel4p@gmail.com>
2022-04-27 14:52:43 -0300

    Teclas de atalhos e adicionado plugin pylsp

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,7 +1,6 @@
 local map = vim.api.nvim_set_keymap
 
-map('n', '<C-b>', ':Buffer<CR>', {noremap = true})          -- Open Buffer fzf
--- map('n', '<C-n>', ':Files<CR>', {noremap = true})          -- Find Files fzf
+map('n', '<C-space>', ':Buffer<CR>', {noremap = true})          -- Open Buffer fzf
 map('n', '<F3>', ':NvimTreeToggle<CR>', {noremap = true})   -- Open File Explorer
 map('n', '<C-r>', ':NvimTreeRefresh<CR>', {noremap = true}) -- Refresh File Explorer
 map('n', '<F5>', ':ToggleTerm<CR>', {noremap = true})         -- Open Terminal
