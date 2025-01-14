633af5e42647cab1eb6453e0450d98563fc0bb95
Israel Pereira <israel4p@gmail.com>
2025-01-14 15:58:34 -0300

    Removida dependencia do luasnip

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -29,8 +29,6 @@ cmp.setup({
     ['<S-Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
