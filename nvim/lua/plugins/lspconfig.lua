31b7337077564b4831cfc3931af672444e2a5043
Israel Pereira <israel4p@gmail.com>
2025-09-23 12:08:27 -0300

    update config deprecate

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,4 @@
-local lspconfig = require('lspconfig')
-
-lspconfig.pyright.setup {
+vim.lsp.config("pyright", {
   settings = {
     python = {
       analysis = {
@@ -11,4 +9,6 @@ lspconfig.pyright.setup {
       }
     }
   }
-}
+})
+
+vim.lsp.enable({"pyright"})
